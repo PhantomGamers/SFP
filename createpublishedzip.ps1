c122d96a7edf192c8faf8b0564a10b4f08c49b6e
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2022-05-16 05:05:05 -0400

    Update createpublishedzip.ps1
    the build was throwing NETSDK1179 despite --self-contained being included. maybe specifying it before specifying --runtime will fix the warning.

--- createpublishedzip.ps1
+++ createpublishedzip.ps1
@@ -10,14 +10,14 @@ function Build-SFP {
     )
 
     Remove-Item -Path "./$configuration/publish" -Recurse -Force -ErrorAction Ignore
-    dotnet publish "SFP_UI/SFP_UI.csproj" --configuration $configuration --output $configuration/publish --runtime $targetRuntime --self-contained
+    dotnet publish "SFP_UI/SFP_UI.csproj" --configuration $configuration --output $configuration/publish --self-contained --runtime $targetRuntime
     # Get-ChildItem "./$configuration/publish/*" -Recurse -Exclude SFP.config,*.log | %{7z.exe a -mx3 "./$configuration/SFP_UI-SelfContained-$targetRuntime.zip" $_.FullName}
     Get-ChildItem "./$configuration/publish/*" -Recurse -Exclude SFP.config,*.log | Compress-Archive -DestinationPath "./$configuration/SFP_UI-SelfContained-$targetRuntime.zip" -Force
 }
 
-foreach ($currentOS in $os.Split(";")) 
+foreach ($currentOS in $os.Split(";"))
 {
     $targetRuntime = "$currentOS-$arch"
-    
+
     Build-SFP -TargetRuntime $targetRuntime
 }
