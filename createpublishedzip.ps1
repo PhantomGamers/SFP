6eb96a7f423a82810e5a39b441649c1497b7da71
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2022-06-12 10:28:02 -0400

    Update createpublishedzip.ps1

--- createpublishedzip.ps1
+++ createpublishedzip.ps1
@@ -14,7 +14,7 @@ function Build-SFP {
     dotnet publish "SFP_UI/SFP_UI.csproj" --configuration $configuration --output $configuration/publish --self-contained --runtime $targetRuntime
     if ($createzip)
     {
-        Get-ChildItem "./$configuration/publish/*" -Recurse -Exclude SFP.config,*.log,FluentAvalonia.pdb | Compress-Archive -DestinationPath "./$configuration/SFP_UI-SelfContained-$targetRuntime.zip" -Force
+        Get-ChildItem "./$configuration/publish/*" -Recurse -Exclude SFP.config,*.log,FluentAvalonia.pdb,FileWatcherEx.pdb | Compress-Archive -DestinationPath "./$configuration/SFP_UI-SelfContained-$targetRuntime.zip" -Force
     }
 }
 
