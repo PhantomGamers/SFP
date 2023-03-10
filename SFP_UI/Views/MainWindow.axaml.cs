5f8007bc8cb931c90eccc687db8d2d9eb6e2d1a0
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2023-03-09 23:37:20 -0500

    Readd Mica effect on Winows 11

--- SFP_UI/Views/MainWindow.axaml.cs
+++ SFP_UI/Views/MainWindow.axaml.cs
@@ -1,20 +1,15 @@
-using System.ComponentModel;
-
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Media.Immutable;
-
+using Avalonia.Styling;
 using FluentAvalonia.Styling;
-using FluentAvalonia.UI.Controls;
 using FluentAvalonia.UI.Media;
 using FluentAvalonia.UI.Windowing;
 
 using SFP.Models;
-
-using SFP_UI.Models;
 
 namespace SFP_UI.Views
 {
@@ -36,6 +31,11 @@ namespace SFP_UI.Views
             }
 
             InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+            Application.Current.ActualThemeVariantChanged += ApplicationActualThemeVariantChanged;
 
             _trayIcon = new TrayIcon
             {
@@ -51,6 +51,56 @@ namespace SFP_UI.Views
             TrayIcon.SetIcons(Application.Current!, icons);
 
             InitializeTrayIcon();
+        }
+
+        private void ApplicationActualThemeVariantChanged(object? sender, EventArgs e)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return;
+            }
+
+            // TODO: add Windows version to CoreWindow
+            if (IsWindows11 && ActualThemeVariant != FluentAvaloniaTheme.HighContrastTheme)
+            {
+                Log.Logger.Info("Enabling Mica effect");
+                TryEnableMicaEffect();
+            }
+            else if (ActualThemeVariant != FluentAvaloniaTheme.HighContrastTheme)
+            {
+                // Clear the local value here, and let the normal styles take over for HighContrast theme
+                SetValue(BackgroundProperty, AvaloniaProperty.UnsetValue);
+            }
+        }
+
+        private void TryEnableMicaEffect()
+        {
+            // The background colors for the Mica brush are still based around SolidBackgroundFillColorBase resource
+            // BUT since we can't control the actual Mica brush color, we have to use the window background to create
+            // the same effect. However, we can't use SolidBackgroundFillColorBase directly since its opaque, and if
+            // we set the opacity the color become lighter than we want. So we take the normal color, darken it and
+            // apply the opacity until we get the roughly the correct color
+            // NOTE that the effect still doesn't look right, but it suffices. Ideally we need access to the Mica
+            // CompositionBrush to properly change the color but I don't know if we can do that or not
+            if (ActualThemeVariant == ThemeVariant.Dark)
+            {
+                Color2 color = this.TryFindResource("SolidBackgroundFillColorBase",
+                    ThemeVariant.Dark, out object? value) ? (Color2)(Color)value : new Color2(32, 32, 32);
+
+                color = color.LightenPercent(-0.8f);
+
+                Background = new ImmutableSolidColorBrush(color, 0.78);
+            }
+            else if (ActualThemeVariant == ThemeVariant.Light)
+            {
+                // Similar effect here
+                Color2 color = this.TryFindResource("SolidBackgroundFillColorBase",
+                    ThemeVariant.Light, out object? value) ? (Color2)(Color)value : new Color2(243, 243, 243);
+
+                color = color.LightenPercent(0.5f);
+
+                Background = new ImmutableSolidColorBrush(color, 0.9);
+            }
         }
 
         protected override void OnOpened(EventArgs e)
@@ -69,6 +119,25 @@ namespace SFP_UI.Views
             {
                 Hide();
             }
+
+            ThemeVariant thm = ActualThemeVariant;
+
+            // Enable Mica on Windows 11
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return;
+            }
+
+            // TODO: add Windows version to CoreWindow
+            if (!IsWindows11 || thm == FluentAvaloniaTheme.HighContrastTheme)
+            {
+                return;
+            }
+
+            TransparencyBackgroundFallback = Brushes.Transparent;
+            TransparencyLevelHint = WindowTransparencyLevel.Mica;
+
+            TryEnableMicaEffect();
         }
 
         protected override void OnClosed(EventArgs e)
@@ -124,9 +193,5 @@ namespace SFP_UI.Views
             WindowState = WindowState.Normal;
             Activate();
         }
-
-        public static bool IsValidRequestedTheme(string thm) => FluentAvaloniaTheme.LightModeString.Equals(thm, StringComparison.OrdinalIgnoreCase) ||
-                FluentAvaloniaTheme.DarkModeString.Equals(thm, StringComparison.OrdinalIgnoreCase) ||
-                FluentAvaloniaTheme.HighContrastModeString.Equals(thm, StringComparison.OrdinalIgnoreCase);
     }
 }
