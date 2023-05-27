fc23ae99600c4440e2842d53d9c1edb0bbd225c1
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2023-05-26 22:02:34 -0400

    use processname from arg

--- SFP/Models/Unix/Utils.cs
+++ SFP/Models/Unix/Utils.cs
@@ -9,9 +9,9 @@ public static class Utils
 {
     public static List<string> GetCommandLine(Process process)
     {
-        const string ProcessName = "steam";
-        const string Command = $"pgrep -x {ProcessName} | xargs -r ps -o command -p | tail -n 1";
-        var output = RunCommand(Command);
+        var processName = process.ProcessName;
+        var command = $"pgrep -x {processName} | xargs -r ps -o command -p | tail -n 1";
+        var output = RunCommand(command);
         var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
         return lines.Select(line => line.Trim()).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
     }
